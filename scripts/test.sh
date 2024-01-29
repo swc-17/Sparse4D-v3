bc836449453ea36d45a9ea31ad62a794686131f4
swc <swc_17@163.com>
2024-01-29 20:51:31 +0800

    init

---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+
+bash ./tools/dist_test.sh \
+    projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py \
+    work_dirs/sparse4dv3_temporal_r50_1x8_bs6_256x704/latest.pth \
+    8 \
+    --eval bbox
---
+++ scripts/train.sh
@@ -0,0 +1,4 @@
+bash ./tools/dist_train.sh \
+   projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py \
+   1 \
+   --deterministic
